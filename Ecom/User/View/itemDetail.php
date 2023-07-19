fb9437871d65848b7219c85c33268ece6517bbe6
thetwaiyanhtet <soe.thi.ha2001@gmail.com>
2023-07-20 02:15:01 +0630

    delete Customer folder

--- Ecom/User/View/itemDetail.php
+++ Ecom/User/View/itemDetail.php
@@ -12,33 +12,33 @@
     <nav>
         <img src="../../image/logo.png" alt="..." class="w-16 ml-5 mt-3">
     </nav>
-    <header class="grid grid-cols-2 justify-items-center">
-        <div class="flex-column ">
+    <header class="grid grid-cols-3">
+        <div class="flex-column bg-slate-300 w-96 rounded-tl-xl rounded-bl-xl space-y-3">
             <div>
                 <img src="../images/image 1.png" alt="...">
             </div>
-            <div class="flex">
+            <div class="flex items-center justify-center space-x-3">
                 <img src="../images/image 2.png" alt="...">
                 <img src="../images/image 3.png" alt="...">
             </div>
         </div>
-        <div>
-            <h2 class="text-4xl">Wide-leg Pants</h2>
+        <div class="w-96 col-start-2 col-span-2">
+            <h2 class="text-4xl ">Wide-leg Pants</h2>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae pariatur nulla atque nisi magnam deleniti
                 illo provident a exercitationem sunt.
             </p>
             <h3>H&M</h3>
             <h1>$749</h1>
-            <div>
-                <span class="w-5 bg-black text-white">a</span>
-                <span class="w-5 bg-black">a</span>
-                <span class="w-5 bg-black">a</span>
-                <span class="w-5 bg-black">a</span>
-                <span class="w-5 bg-black">a</span>
+            <div class="flex space-x-3">
+                <div class="w-5 h-5 bg-black text-white">a</div>
+                <div class="w-5 h-5 bg-black">a</div>
+                <div class="w-5 h-5 bg-black">a</div>
+                <div class="w-5 h-5 bg-black">a</div>
+                <div class="w-5 h-5 bg-black">a</div>
             </div>
             <div>
-                Quantity <input type="number">
+                Quantity <input type="number" class="border">
             </div>
 
         </div>
