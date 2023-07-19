3b2d1d37dd93ec4d4aa15e52344ea253b201fd07
thetwaiyanhtet <soe.thi.ha2001@gmail.com>
2023-07-20 01:26:45 +0630

    ..

---
+++ Ecom/User/View/itemDetail.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Item Detail</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <nav>
+        <img src="../../image/logo.png" alt="..." class="w-16 ml-5 mt-3">
+    </nav>
+    <header class="grid grid-cols-2 justify-items-center">
+        <div class="flex-column ">
+            <div>
+                <img src="../images/image 1.png" alt="...">
+            </div>
+            <div class="flex">
+                <img src="../images/image 2.png" alt="...">
+                <img src="../images/image 3.png" alt="...">
+            </div>
+        </div>
+        <div>
+            <h2>Wide-leg Pants</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae pariatur nulla atque nisi magnam deleniti
+                illo provident a exercitationem sunt.
+            </p>
+            <h3>H&M</h3>
+            <h1>$749</h1>
+            <div>
+                <span class="w-5 bg-black text-white">a</span>
+                <span class="w-5 bg-black">a</span>
+                <span class="w-5 bg-black">a</span>
+                <span class="w-5 bg-black">a</span>
+                <span class="w-5 bg-black">a</span>
+            </div>
+            Quantity <input type="number">
+
+        </div>
+    </header>
+
+
+</body>
+
+</html>
