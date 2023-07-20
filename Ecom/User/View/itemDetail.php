6e5bdfa540b6f1f6fa2932326ea864e1c0286776
thetwaiyanhtet <soe.thi.ha2001@gmail.com>
2023-07-20 12:25:02 +0630

    ..

--- Ecom/User/View/itemDetail.php
+++ Ecom/User/View/itemDetail.php
@@ -5,15 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Item Detail</title>
-    <link rel="stylesheet" href="../dist/output.css">
+    <link rel="stylesheet" href="./resources/lib/tailwind/output.css">
 </head>
 
 <body class="bg-indigo-300">
     <nav>
         <img src="../../image/logo.png" alt="..." class="w-16 ml-5 mt-3">
     </nav>
-    <header class="grid grid-cols-2 justify-items-center">
-        <div class="flex-column bg-slate-200 w-96 rounded-tl-xl rounded-bl-xl space-y-3">
+    <header class="grid grid-cols-3 justify-items-center">
+        <div class="flex-column bg-slate-600 w-96 rounded-tl-xl rounded-bl-xl space-y-3">
             <div>
                 <img src="../images/image 1.png" alt="...">
             </div>
@@ -22,8 +22,8 @@
                 <img src="../images/image 3.png" alt="...">
             </div>
         </div>
-        <div class="w-[500px] bg-slate-200 rounded-xl grid">
-            <h2 class="text-4xl ">Wide-leg Pants</h2>
+        <div class="w-[500px] bg-slate-200 rounded-xl grid col-start-2 col-span-2">
+            <h2 class="text-6xl ">Wide-leg Pants</h2>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae pariatur nulla atque nisi magnam deleniti
                 illo provident a exercitationem sunt.
--- Ecom/User/View/testing.php
+++ Ecom/User/View/testing.php
@@ -9,6 +9,6 @@
   </head>
 
   <body>
-    <h1 class="">Hello World</h1>
+    <h1 class="underline text-3xl">Hello World</h1>
   </body>
 </html>
