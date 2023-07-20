0547dc2a50f3230ab13cef10bd5ef68bec6f35f2
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-07-21 01:37:01 +0630

    Update cart2.php

--- Ecom/User/View/cart2.php
+++ Ecom/User/View/cart2.php
@@ -1,82 +1,84 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Philosopher&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Philosopher&display=swap" rel="stylesheet">
     <link href="./resources/lib/tailwind/output.css" rel="stylesheet">
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <title>cart</title>
 </head>
+
 <body>
     <!-- component -->
-<div class="h-full w-full py-16">
-<!--- more free and premium Tailwind CSS components at https://tailwinduikit.com/ --->
+    <div class="h-full w-full py-16">
+        <!--- more free and premium Tailwind CSS components at https://tailwinduikit.com/ --->
 
-            <div class="container  mx-auto">
-                <!--Code Block for progress bar Starts-->
-                <dh-component>
-                    <div class=" w-11/12 lg:w-2/6 mx-auto">
-                        <div class="bg-gray-200 h-1 flex items-center justify-between relative">
+        <div class="container  mx-auto">
+            <!--Code Block for progress bar Starts-->
+            <dh-component>
+                <div class=" w-11/12 lg:w-2/6 mx-auto">
+                    <div class="bg-gray-200 h-1 flex items-center justify-between relative">
                         <div class="absolute left-0 -mr-4">
-                                    <div class="relative px-2 py-1 rounded mt-20 -mr-12">
-                                        <p tabindex="0" class="focus:outline-none text-xl font-semibold ">Cart</p>
-                                    </div>
-                                </div>
-                            <div class="w-1/3 bg-gray-200 h-3 flex items-center relative">
+                            <div class="relative px-2 py-1 rounded mt-20 -mr-12">
+                                <p tabindex="0" class="focus:outline-none text-xl font-semibold ">Cart</p>
+                            </div>
+                        </div>
+                        <div class="w-1/3 bg-gray-200 h-3 flex items-center relative">
                             <div class="absolute right-0 -mr-2">
-                                    <div class="relative px-2 py-1 rounded mt-20 -mr-12">
-                                        <p tabindex="0" class="focus:outline-none text-xl font-semibold">Delivery</p>
-                                    </div>
-                                </div>
-                                <div class=" bg-gray-200 h-10 w-10 rounded-full border shadow flex items-center justify-center">
-                                <ion-icon class="text-3xl" name="cart-outline"></ion-icon>
+                                <div class="relative px-2 py-1 rounded mt-20 -mr-12">
+                                    <p tabindex="0" class="focus:outline-none text-xl font-semibold">Delivery</p>
                                 </div>
                             </div>
-                         
-                            <div class="w-1/3 flex justify-between bg-gray-200 h-3 items-center relative">
-                                <div class="absolute right-0 -mr-2">
-                                    <div class="relative  px-2 py-1 rounded mt-20 -mr-3">
-                                        <p tabindex="0" class="focus:outline-none  text-xl font-semibold">Card</p>
-                                    </div>
+                            <div class=" bg-gray-200 h-10 w-10 rounded-full border shadow flex items-center justify-center">
+                                <ion-icon class="text-3xl" name="cart-outline"></ion-icon>
+                            </div>
+                        </div>
+
+                        <div class="w-1/3 flex justify-between bg-gray-200 h-3 items-center relative">
+                            <div class="absolute right-0 -mr-2">
+                                <div class="relative  px-2 py-1 rounded mt-20 -mr-3">
+                                    <p tabindex="0" class="focus:outline-none  text-xl font-semibold">Card</p>
                                 </div>
-                                <div class="bg-gray-200 h-10 w-10 rounded-full shadow flex items-center justify-center -ml-2">
+                            </div>
+                            <div class="bg-gray-200 h-10 w-10 rounded-full shadow flex items-center justify-center -ml-2">
                                 <ion-icon class="text-3xl" name="car-outline"></ion-icon>
-                                </div>
-                                <div class="w-1/5 bg-gray-200 h-3 flex items-center relative">
+                            </div>
+                            <div class="w-1/5 bg-gray-200 h-3 flex items-center relative">
                                 <div class="bg-gray-200 h-10 w-10 rounded-full shadow flex items-center justify-center -mr-3 relative">
                                     <ion-icon class="text-3xl" name="card-outline"></ion-icon>
                                 </div>
-                                </div>
-                               
                             </div>
-                           
-                            <div class="w-1/3 flex h-3 bg-gray-200 items-center justify-end relative">
-                                <div class="bg-gray-200 h-10 w-10 rounded-full shadow flex items-center justify-center">
-                                <ion-icon class="text-3xl " name="checkmark-circle-outline"></ion-icon>
-                                </div>
-                                <div class="absolute right-0 -mr-2">
-                                    <div class="relative  px-2 py-1 rounded mt-20 -mr-10">
-                                        <p tabindex="0" class="focus:outline-none  text-xl font-semibold">Confirmation</p>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            
-                        </div>
-                    </div>
 
-                </dh-component>
-                <!--Code Block for progress bar Starts-->
-            </div>
-           
+                        </div>
+
+                        <div class="w-1/3 flex h-3 bg-gray-200 items-center justify-end relative">
+                            <div class="bg-gray-200 h-10 w-10 rounded-full shadow flex items-center justify-center">
+                                <ion-icon class="text-3xl " name="checkmark-circle-outline"></ion-icon>
+                            </div>
+                            <div class="absolute right-0 -mr-2">
+                                <div class="relative  px-2 py-1 rounded mt-20 -mr-10">
+                                    <p tabindex="0" class="focus:outline-none  text-xl font-semibold">Confirmation</p>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </dh-component>
+            <!--Code Block for progress bar Starts-->
         </div>
-        <div>
-                <p>Shopping Bag</p>
-            </div>
+
+    </div>
+    <div>
+        <p>Shopping Bag</p>
+    </div>
 </body>
+
 </html>
