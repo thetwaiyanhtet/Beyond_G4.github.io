bc806b29960fa36cfe4f44c85ca081d6d22c8f9b
thetwaiyanhtet <soe.thi.ha2001@gmail.com>
2023-07-21 16:07:54 +0630

    ..

---
+++ Ecom/User/View/profileMenu.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile Menu</title>
+    <link rel="stylesheet" href="./resources/lib/tailwind/output.css">
+</head>
+
+<body>
+    <main class=" min-h-screen flex items-center justify-center">
+        <div class="flex justify-center items-center space-x-5 w-[800px] bg-amber-200 p-10">
+            <div class=" w-[350px] h-[500px] bg-slate-500 flex flex-col justify-center items-center space-y-7 rounded-lg">
+                <img src="./resources/img/bille.png" alt="..." class=" w-18 rounded-lg">
+                <div class="text-center">
+                    <p>NAME</p>
+                    <p>Username</p>
+                </div>
+                <div class="">
+                    <div class=" flex  justify-evenly">
+                        <p>Email</p>
+                        <p>uer***@gmail.com</p>
+                    </div>
+                    <div class=" flex justify-around items-start">
+                        <p>Ph_no</p>
+                        <p>09*******</p>
+                    </div>
+                    <div class=" flex justify-around items-start">
+                        <p>Current Address</p>
+                        <p>Yangon</p>
+                    </div>
+                </div>
+                <button class=" text-black px-5 py-2 shadow-lg rounded-md bg-white">Log out</button>
+            </div>
+            <div class=" w-[350px] h-[500px] bg-slate-500"></div>
+        </div>
+    </main>
+</body>
+
+</html>
