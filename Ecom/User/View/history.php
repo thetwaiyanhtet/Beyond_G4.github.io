8cfa13ba9c9a8d2d625f0ffc2b6ac082498211cc
nyanwinmyo <nyanwinmyo@gmail.com>
2023-07-21 20:23:43 +0630

    update

--- Ecom/User/View/history.php
+++ Ecom/User/View/history.php
@@ -20,7 +20,7 @@
             <p class="text-3xl ml-[1%] max-md:text-2xl font-philosopher font-bold ">Beyond</p>
         </span>
         <span>
-            <ion-icon class="text-7xl float-right " name="person-circle-outline"></ion-icon>
+            <ion-icon class="text-4xl lg:text-4xl float-right " name="person-circle-outline"></ion-icon>
         </span>
     </section>
     <section class="relative">
@@ -38,58 +38,58 @@
                     <span class="hidden lg:block text-3xl font-semibold">Gymshark</span>
                 </div>
                 <div class="flex flex-col item-center text-center">
-                    <p class="text-sm lg:text-2xl text-white lg:font-bold">Product</p>
-                    <p class="text-sm lg:text-2xl text-white mt-3 lg:mt-9 lg:font-bold ">Crop Tube</p>
+                    <p class="text-xs lg:text-xl text-white lg:font-semibold">Product</p>
+                    <p class="text-xs lg:text-xl text-white mt-3 lg:mt-9 lg:font-semibold ">Crop Tube</p>
                 </div>
                 <div class="flex flex-col item-center text-center">
-                    <p class="text-sm lg:text-2xl text-white lg:font-bold">Order Date</p>
-                    <p class="text-sm lg:text-2xl text-white mt-3 lg:mt-9 lg:font-bold">11/07/2023</p>
+                    <p class="text-xs lg:text-xl text-white lg:font-semibold">Order Date</p>
+                    <p class="text-xs lg:text-xl text-white mt-3 lg:mt-9 lg:font-semibold">11/07/2023</p>
                 </div>
                 <div class="flex flex-col item-center text-center">
-                    <p class="text-sm lg:text-2xl text-white lg:font-bold">Quantity</p>
-                    <p class="text-sm lg:text-2xl text-white mt-3 lg:mt-9 lg:font-bold">1</p>
+                    <p class="text-xs lg:text-xl text-white lg:font-semibold">Quantity</p>
+                    <p class="text-xs lg:text-xl text-white mt-3 lg:mt-9 lg:font-semibold">1</p>
                 </div>
                 <div class="flex flex-col item-center text-center">
-                    <p class="text-sm lg:text-2xl text-white lg:font-bold">Total Price</p>
-                    <p class="text-sm lg:text-2xl text-white mt-3 lg:mt-9 lg:font-bold">$200</p>
+                    <p class="text-xs lg:text-xl text-white lg:font-semibold">Total Price</p>
+                    <p class="text-xs lg:text-xl text-white mt-3 lg:mt-9 lg:font-semibold">$200</p>
                 </div>
                 <button class="w-[38px] lg:w-[113px] h-[16px] lg:h-[40px] rounded-md font-philosopher bg-[#eeeeeeae]
-                 text-black text-sm lg:text-3xl lg:mt-9 mt-7 ">view</button>
+                 text-black text-xs lg:text-3xl lg:mt-9 mt-5 ">view</button>
             </div>
         </div>
 
-        <div class="absolute w-[362px] lg:w-[1043px] h-[103px] lg:h-[154px] bg-[#AC8CE0] border border-1 border-black top-[38%] lg:top-[55%] left-[9%] lg:left-[17%]"></div>
-        <div class="absolute w-[362px] lg:w-[1043px] h-[103px] lg:h-[154px] bg-[#AC8CE0] border border-1 border-black top-[36%] lg:top-[53%] left-[8%] lg:left-[16%]"></div>
-        <div class="absolute w-[362px] lg:w-[1043px] h-[103px] lg:h-[154px] bg-[#AC8CE0] border border-1 border-black top-[34%] lg:top-[51%] left-[7%] lg:left-[15%]">
-        <div class="flex justify-evenly mt-7 lg:mt-8">
+        <div class="absolute mt-8 lg:mt-0 w-[362px] lg:w-[1043px] h-[103px] lg:h-[154px] bg-[#AC8CE0] border border-1 border-black top-[38%] lg:top-[55%] left-[9%] lg:left-[17%]"></div>
+        <div class="absolute mt-8 lg:mt-0 w-[362px] lg:w-[1043px] h-[103px] lg:h-[154px] bg-[#AC8CE0] border border-1 border-black top-[36%] lg:top-[53%] left-[8%] lg:left-[16%]"></div>
+        <div class="absolute mt-8 lg:mt-0 w-[362px] lg:w-[1043px] h-[103px] lg:h-[154px] bg-[#AC8CE0] border border-1 border-black top-[34%] lg:top-[51%] left-[7%] lg:left-[15%]">
+        <div class="flex justify-evenly mt-7 lg:mt-8 ">
                 <div class="flex items-center">
                     <img class=" w-10 lg:w-24 " src="../View/resources/img/gymshark.png" alt="">
                     <span class="hidden lg:block text-3xl font-semibold">Gymshark</span>
                 </div>
                 <div class="flex flex-col item-center text-center">
-                    <p class="text-sm lg:text-2xl text-white lg:font-bold">Product</p>
-                    <p class="text-sm lg:text-2xl text-white mt-3 lg:mt-9 lg:font-bold ">Crop Tube</p>
+                    <p class="text-xs lg:text-xl text-white lg:font-semibold">Product</p>
+                    <p class="text-xs lg:text-xl text-white mt-3 lg:mt-9 lg:font-semibold ">Crop Tube</p>
                 </div>
                 <div class="flex flex-col item-center text-center">
-                    <p class="text-sm lg:text-2xl text-white lg:font-bold">Order Date</p>
-                    <p class="text-sm lg:text-2xl text-white mt-3 lg:mt-9 lg:font-bold">11/07/2023</p>
+                    <p class="text-xs lg:text-xl text-white lg:font-semibold">Order Date</p>
+                    <p class="text-xs lg:text-xl text-white mt-3 lg:mt-9 lg:font-semibold">11/07/2023</p>
                 </div>
                 <div class="flex flex-col item-center text-center">
-                    <p class="text-sm lg:text-2xl text-white lg:font-bold">Quantity</p>
-                    <p class="text-sm lg:text-2xl text-white mt-3 lg:mt-9 lg:font-bold">1</p>
+                    <p class="text-xs lg:text-xl text-white lg:font-semibold">Quantity</p>
+                    <p class="text-xs lg:text-xl text-white mt-3 lg:mt-9 lg:font-semibold">1</p>
                 </div>
                 <div class="flex flex-col item-center text-center">
-                    <p class="text-sm lg:text-2xl text-white lg:font-bold">Total Price</p>
-                    <p class="text-sm lg:text-2xl text-white mt-3 lg:mt-9 lg:font-bold">$200</p>
+                    <p class="text-xs lg:text-xl text-white lg:font-semibold">Total Price</p>
+                    <p class="text-xs lg:text-xl text-white mt-3 lg:mt-9 lg:font-semibold">$200</p>
                 </div>
                 <button class="w-[38px] lg:w-[113px] h-[16px] lg:h-[40px] rounded-md font-philosopher bg-[#eeeeeeae]
-                 text-black text-sm lg:text-3xl lg:mt-9 mt-7">view</button>
+                 text-black text-xs lg:text-3xl lg:mt-9 mt-5">view</button>
             </div>
         </div>
 
 
         <ion-icon
-            class=" lg:text-6xl absolute left-[5%] top-[78%] cursor-pointer max-md:text-7xl max-md:left-8 max-md:bottom-24"
+            class=" lg:text-6xl absolute left-[5%] top-[72%] cursor-pointer max-md:text-7xl max-md:left-8 max-md:bottom-24"
             name="caret-back-outline"></ion-icon>
     </section>
 
---
+++ Ecom/User/View/historyDetail.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./resources/lib/tailwind/output.css" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Philosopher:wght@700&display=swap" rel="stylesheet">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</head>
+
+<body class="mt-3 font-poppins overflow-hidden">
+    <section class=" flex items-center justify-between mx-[3%]">
+        <span class="flex items-center  my-[1%]">
+            <img class=" w-16 lg:w-20" src="../View/resources/img/logo.png" alt="">
+            <p class="text-2xl lg:text-4xl ml-[1%] max-md:text-2xl font-philosopher font-bold ">Beyond</p>
+        </span>
+        <span>
+            <ion-icon class="text-4xl float-right " name="person-circle-outline"></ion-icon>
+        </span>
+    </section>
+    <section class="relative">
+        <section class="w-screen  h-screen rounded-tl-[50px] rounded-tr-[50px] 
+              bg-gradient-to-tl from-[#d5a0e6] - to-[#9f80f4] min-h-screen flex items-center justify-center">
+            <div class="lg:w-[948px] h-[466px] lg:h-[400px] bg-[#ffffff41] mb-72 lg:mt-10 rounded-3xl shadow-inner  flex items-center lg:justify-evenly lg:flex-row flex-col ">
+                <div class="pt-10 lg:pt-0">
+                    <div class="w-[250px] h-[200px] lg:h-[248px] bg-[#fffbfb47] rounded-3xl flex items-center border ">
+                        <img src="../View/resources/img/black girl blouse.png" alt="">
+                    </div>
+                </div>
+                <div class="flex flex-col text-white w-[354px] h-[295px] mb-16  pt-9 p-16  lg:pt-0 lg:pl-0">
+                    <p class="font-semibold text-[15px] lg:text-[36px] lg:text-center">Crop Tube</p>
+                    <div class="flex font-medium text-[13px] lg:text-[24px] pt-5">
+                        <p>Color:</p>
+                        <p class="ml-[100px] lg:ml-[130px]">Black</p>
+                    </div>
+                    <div class="flex font-medium text-[13px] lg:text-[24px] pt-2">
+                        <p>Size:</p>
+                        <p class="ml-[110px] lg:ml-[150px]">M</p>
+                    </div>
+                    <div class="flex font-medium text-[13px] lg:text-[24px] pt-2">
+                        <p>Total Count:</p>
+                        <p class="ml-[60px] lg:ml-[60px]">1</p>
+                    </div>
+                    <div class="flex font-medium text-[13px] lg:text-[24px] pt-2">
+                        <p>Date:</p>
+                        <p class="ml-[105px] lg:ml-[140px]">11/07/2023</p>
+                    </div>
+                    <div class="flex font-medium text-[13px] lg:text-[24px] pt-2">
+                        <p>Total Price:</p>
+                        <p class="ml-[70px] lg:ml-[75px]">$200</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <ion-icon class=" lg:text-7xl absolute left-[5%] top-[80%] lg:top-[75%] cursor-pointer max-md:text-6xl max-md:left-8 max-md:bottom-24" name="caret-back-outline"></ion-icon>
+    </section>
+
+</body>
+
+</html>
