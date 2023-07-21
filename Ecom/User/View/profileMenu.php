4f4e49e0cacfc8fd85d78f118793aeb46c2497eb
thetwaiyanhtet <soe.thi.ha2001@gmail.com>
2023-07-22 00:55:26 +0630

    final profile menu

--- Ecom/User/View/profileMenu.php
+++ Ecom/User/View/profileMenu.php
@@ -14,10 +14,12 @@
     <link href="https://fonts.googleapis.com/css2?family=Philosopher:wght@700&display=swap" rel="stylesheet">
 </head>
 
-<body class=" bg-violet-200 font-philosopher letter overflow-hidden relative">
-    <main class=" min-h-screen flex items-center justify-center">
-        <div class="flex justify-center items-center space-x-5 w-[800px] bgtransparent border shadow-2xl py-10 px-20 rounded-3xl z-50">
-            <div class=" font-poppins w-[380px] h-[400px] bg-white flex flex-col justify-center items-center space-y-7 rounded-lg text-sm p-5">
+<body class=" bg-violet-200 font-philosopher letter relative">
+    <main class=" min-h-screen flex items-center justify-center overflow-hidden">
+
+        <div class="lg:flex justify-center items-center lg:space-x-5 lg:w-[800px] w-[380px] h-min-screen bg-transparent border shadow-2xl lg:py-10 lg:px-20 py-5 px-16 rounded-3xl z-50">
+
+            <div class=" font-poppins lg:w-[380px] w-[250px] h-[400px] bg-white flex flex-col justify-center items-center space-y-7 rounded-lg lg:text-sm lg:p-5 p-2 text-xs">
                 <img src="./resources/img/bille.png" alt="..." class=" w-16 rounded-lg">
                 <div class="text-center">
                     <p class=" text-2xl font-philosopher">NAME</p>
@@ -39,41 +41,39 @@
                 </div>
                 <button class=" text-black px-5 py-2 shadow-xl rounded-md bg-white">Log out</button>
             </div>
-            <div class=" w-[350px] h-[400px] flex-col space-y-3">
-                <div class=" settingBar mt-4">
-                    <img src="./resources/img/manage-accounts.svg" alt="..." width="30px">
+
+            <div class=" lg:w-[350px] lg:h-[400px] lg:flex-col justify-center items-center flex flex-wrap space-y-3 lg:text-base text-xs lg:space-x-0 space-x-3">
+                <div class=" flex justify-around items-center lg:py-4 lg:px-2 py-2 border bg-white rounded-xl shadow-lg mt-4 lg:w-[290px] w-[160px]">
+                    <img src="./resources/img/manage-accounts.svg" alt="..." class=" lg:w-[30px] w-[20px]">
                     <p class=" uppercase">manage profile</p>
-                    <ion-icon name="chevron-forward-outline" class=" text-2xl"></ion-icon>
+                    <ion-icon name="chevron-forward-outline" class=" lg:text-2xl text-base"></ion-icon>
                 </div>
                 <div class=" settingBar">
-                    <img src="./resources/img/cart-outline.svg" alt="..." width="30px">
-                    <p class=" uppercase">manage profile</p>
-                    <ion-icon name="chevron-forward-outline" class=" text-2xl"></ion-icon>
+                    <img src="./resources/img/cart-outline.svg" alt="..." class=" lg:w-[30px] w-[20px]">
+                    <p class=" uppercase">cart</p>
+                    <ion-icon name="chevron-forward-outline" class=" lg:text-2xl text-base"></ion-icon>
                 </div>
                 <div class=" settingBar">
-                    <img src="./resources/img/heart-line.svg" alt="..." width="30px">
-                    <p class=" uppercase">manage profile</p>
-                    <ion-icon name="chevron-forward-outline" class=" text-2xl"></ion-icon>
+                    <img src="./resources/img/heart-line.svg" alt="..." class=" lg:w-[30px] w-[20px]">
+                    <p class=" uppercase">wishlist</p>
+                    <ion-icon name="chevron-forward-outline" class=" lg:text-2xl text-base"></ion-icon>
                 </div>
                 <div class=" settingBar">
-                    <img src="./resources/img/history.svg" alt="..." width="30px">
-                    <p class=" uppercase">manage profile</p>
-                    <ion-icon name="chevron-forward-outline" class=" text-2xl"></ion-icon>
+                    <img src="./resources/img/history.svg" alt="..." class=" lg:w-[30px] w-[20px]">
+                    <p class=" uppercase">history</p>
+                    <ion-icon name="chevron-forward-outline" class=" lg:text-2xl text-base"></ion-icon>
                 </div>
                 <div class=" settingBar">
-                    <img src="./resources/img/notifications-outline.svg" alt="..." width="30px">
-                    <p class=" uppercase">manage profile</p>
-                    <ion-icon name="chevron-forward-outline" class=" text-2xl"></ion-icon>
+                    <img src="./resources/img/notifications-outline.svg" alt="..." class=" lg:w-[30px] w-[20px]">
+                    <p class=" uppercase">notification</p>
+                    <ion-icon name="chevron-forward-outline" class=" lg:text-2xl text-base"></ion-icon>
                 </div>
             </div>
+
         </div>
-        <div class="lg:block hidden absolute bottom-[10px] left-[220px] col-start-2 text-start"><ion-icon name="caret-back-outline" class="w-10 h-10"></ion-icon></div>
 
-        <!-- <div class=" bg-purple-400 w-52 h-52 rounded-lg border-2"></div>
-        <div class=" bg-purple-400 w-36 h-36 rounded-lg border-2"></div>
-        <div class=" bg-purple-400 w-14 h-14 rounded-lg border-2"></div> -->
-
-        <img src="./resources/img/bg.png" alt="..." class=" absolute -right-44 bottom-0">
+        <div class=" absolute bottom-[10px] lg:left-[220px] left-2 col-start-2 text-start"><ion-icon name="caret-back-outline" class="w-10 h-10"></ion-icon></div>
+        <img src="./resources/img/bg.png" alt="..." class=" absolute lg:-right-44 bottom-0 lg:w-[600px] w-96">
     </main>
 </body>
 
