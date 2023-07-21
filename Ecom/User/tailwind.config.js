6e3f94cb2191ca8cdf61f3bdcfb77cfa62823b1d
Hnin Eain Latt <>
2023-07-22 02:31:17 +0630

    update

--- Ecom/User/tailwind.config.js
+++ Ecom/User/tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     extend: {
       colors:{
         'my-purple1' : '#a78bfa',
-        'my-purple2' : '#8b5cf6'
+        'my-purple2' : '#8b5cf6',
+        "btn-color"  :  '#314755',
+        "verify-color" : '#D9D9D9',
+        "cart-color" :'#FDFBFB',
+        "btn2-color" :'#F09578',
+        'border-color' : '#9A9A9A',
+        'text-color' : '#314755'
       },
       fontFamily:{
         'philosopher' : ['Philosopher', 'sans-serif'],
