7e6a3de9d21110514feb71b8b19344a757f352cf
thetwaiyanhtet <soe.thi.ha2001@gmail.com>
2023-07-23 23:21:04 +0630

    ...

--- Ecom/User/View/itemDetail.php
+++ Ecom/User/View/itemDetail.php
@@ -67,7 +67,7 @@
         </header>
         <div class="grid grid-cols-6">
             <div class="lg:block hidden absolute bottom-[90px] left-[220px] col-start-2 text-start"><ion-icon name="caret-back-outline" class="w-10 h-10"></ion-icon></div>
-            <div class=" fixed right-[50px] bottom-10 col-start-5 ml-36 w-14 h-14 bg-white rounded-lg flex justify-center items-center z-50 shadow-inner"><ion-icon name="chatbubble-ellipses-outline" class="w-10 h-10"></ion-icon></div>
+            <div class=" fixed lg:right-[50px] right-[20px] bottom-10 col-start-5 ml-36 w-14 h-14 bg-white rounded-lg flex justify-center items-center z-50 shadow-inner"><ion-icon name="chatbubble-ellipses-outline" class="w-10 h-10"></ion-icon></div>
         </div>
 
         <main class="flex justify-center">
