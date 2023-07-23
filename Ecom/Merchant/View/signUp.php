1669998f85d7b2aa1ee3b88ca21b2f39aa144457
nyanwinmyo <nyanwinmyo@gmail.com>
2023-07-23 18:37:01 +0630

    merchant update

---
+++ Ecom/Merchant/View/signUp.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./resources/lib/tailwind/output.css" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Philosopher:wght@700&display=swap" rel="stylesheet">
+
+
+</head>
+
+<body>
+    <h1 class=" underline italic text-blue-700 font-bold text-4xl">Hello</h1>
+</body>
+
+</html>
