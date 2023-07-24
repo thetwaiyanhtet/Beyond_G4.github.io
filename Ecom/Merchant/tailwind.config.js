841105927c59a22d68dbdd82148c4c820d6dcc85
thetwaiyanhtet <soe.thi.ha2001@gmail.com>
2023-07-24 16:06:28 +0630

    side bar position fix

--- Ecom/Merchant/tailwind.config.js
+++ Ecom/Merchant/tailwind.config.js
@@ -3,10 +3,15 @@ module.exports = {
   content: ["./View/*.{html,js,php}"],
   theme: {
     extend: {
-      colors:{
-          'sidebar' : "#085FC6",
-      }
+      colors: {
+        sidebar: "#085FC6",
+      },
+      fontFamily: {
+        philosopher: ["Philosopher", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        PlayfairSC: ["Playfair Display SC", "serif"],
+      },
     },
   },
   plugins: [],
-}
+};
