c09f3f087ad3d14a6c2ea6644bc183a1d5c946f9
Hninaeinlatt <Hninaeinlatt438@gmail.com>
2023-07-27 00:01:33 +0630

    hel Upate

--- Ecom/Merchant/View/Invoice.php
+++ Ecom/Merchant/View/Invoice.php
@@ -1,3 +1,6 @@
+<?php
+include "./sidebar.php"
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,7 +17,66 @@
 </head>
 
 <body>
-    
+    <main class="ml-56">
+        <header class=" border-gray-300 border-b-2 h-[82px] flex justify-between items-center">
+            <div class=" pl-2">
+                <p>2023/July/24 - 4:04 PM</p>
+            </div>
+            <div>
+                <ul class="flex space-x-5 items-center pr-5">
+                    <li><ion-icon name="moon-outline" class=" text-xl"></ion-icon></li>
+                    <li><ion-icon name="notifications-outline" class=" text-xl"></ion-icon></li>
+                    <li><img src="./resources/img/amazfit.png" alt="..." width="60px"></li>
+                </ul>
+            </div>
+        </header>
+        <p class=" font-poppins m-2">Order > Invoice </p>
+        <div class=" w-[400px] h-[730px] rounded-lg border border-solid m-auto mt-5 mb-5 bg-bg-color">
+            <p class=" font-poppins text-center font-bold text-lg mt-5">Thank you for Your Business</p>
+            <p class="text-center text-base mt-1">Your Order</p>
+            <p class="text-center text-sm text-gray-400  mt-1">Friday , July 14 2023 at 4:04 PM</p>
+            <div class="m-auto w-64 border-solid border-b-2 border-gray-300 flex justify-between mt-5"></div>
+            <div class="m-auto w-64 border-solid border-b-2 border-gray-300 flex justify-between mt-3">
+                <p class="mb-3">Something</p>
+                <p class="mb-3">$20.00</p>
+            </div>
+            <div class="m-auto w-64 border-solid border-b-2 border-gray-300 flex justify-between mt-3">
+                <p class="mb-3">Something</p>
+                <p class="mb-3">$20.00</p>
+            </div>
+            <div class="m-auto w-64 border-solid border-b-2 border-gray-300 flex justify-between mt-3">
+                <p class="mb-3">Something</p>
+                <p class="mb-3">$20.00</p>
+            </div>
+            <div class="m-auto w-64 border-solid border-b-2 border-gray-300 flex justify-between mt-3">
+                <p class="mb-3">Sub Total</p>
+                <p class="mb-3">$20.00</p>
+            </div>
+            <div class="m-auto w-64 border-solid border-b-2 border-gray-300 flex justify-between mt-3">
+                <p class="mb-3">Tax</p>
+                <p class="mb-3">$20.00</p>
+            </div>
+            <div class="m-auto w-64 border-solid border-b-2 border-black flex justify-between mt-3">
+                <p class="mb-3">Delivery Fee</p>
+                <p class="mb-3">$20.00</p>
+            </div>
+            <div class="m-auto w-64 border-solid border-b-2 border-black flex justify-between mt-3">
+                <p class="mb-3">Total</p>
+                <p class="mb-3">$120.00</p>
+            </div>
+            <p class="text-center font-bold text-lg m-5">Your Details</p>
+
+            <div class="m-auto w-80 border-solid border-b-2 border-t-2 border-gray-300 flex justify-between mt-3">
+                <p class="m-3 text-justify font-normal">Shipping to</p>
+                <p class="m-3 text-sm text-center">$No.31 One Street,Pyay Road,Yangon</p>
+            </div>
+            <div class="m-auto w-80 border-solid border-b-2 border-gray-300 flex justify-between mt-3">
+                <p class="m-3 text-justify font-normal">Billed With</p>
+                <p class="m-3">Kpay</p>
+            </div>
+            <p class="text-center mt-2">Thank You For Being a Great Customer.</p>
+        </div>
+    </main>
 
 </body>
 
---
+++ Ecom/Merchant/View/orderDetail.php
@@ -0,0 +1,135 @@
+<?php
+include "./sidebar.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order Detail</title>
+    <link href="./resources/lib/tailwind/output.css?id=<?= time() ?>" rel="stylesheet">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <main class="ml-56">
+        <header class=" border-gray-300 border-b-2 h-[82px] flex justify-between items-center">
+            <div class=" pl-2">
+                <p>2023/July/24 - 4:04 PM</p>
+            </div>
+            <div>
+                <ul class="flex space-x-5 items-center pr-5">
+                    <li><ion-icon name="moon-outline" class=" text-xl"></ion-icon></li>
+                    <li><ion-icon name="notifications-outline" class=" text-xl"></ion-icon></li>
+                    <li><img src="./resources/img/amazfit.png" alt="..." width="60px"></li>
+                </ul>
+            </div>
+        </header>
+        <div class="flex justify-between">
+            <div class="flex m-2">
+                <img src="./resources/img/fluent_people-32-filled.png" alt="" class="h-8">
+                <p class="m-2">Order Details</p>
+            </div>
+            <div class="m-2">
+                <input type="checkbox" name="" id="">
+                <label for="">Invoice</label>
+            </div>
+        </div>
+        <div class="flex justify-between">
+            <!--Order Details-->
+            <div class="w-96 h-56 border border-solid drop-shadow-xl m-5 ml-10">
+                <p class="m-2">Order Details</p>
+                <hr>
+                <div class="m-2">
+                    <p class="m-2">Name:User</p>
+                    <p class="m-2">Order Date: 12-04-2023</p>
+                    <p class="m-2">Total Amount: $1500.00</p>
+                    <p class="m-2">Payment:Bank Deposit</p>
+                </div>
+            </div>
+            <!--Delivery-->
+            <div class="w-96 h-64 border border-solid drop-shadow-xl m-4">
+                <p class="m-2">Delivery Address</p>
+                <hr>
+                <div class="m-2">
+                    <p class="m-2">Name:User</p>
+                    <p class="m-2">Phone: 09587746358</p>
+                    <p class="m-2">Address: Pyay Road</p>
+                    <p class="m-2">City: Yangon</p>
+                    <p class="m-2">Delivery services: Royal Express</p>
+                    <p class="m-2">Delivery charges: $10.00 </p>
+                </div>
+            </div>
+        </div>
+        <!--Product Summary-->
+        <div class="w-[900px] h-[390px] border border-solid drop-shadow-xl ml-10">
+            <p class="m-5">Product Summary</p>
+            <table class="table-auto w-full">
+                <thead>
+                    <tr class="text-center">
+                        <th class="text-center px-5">No</th>
+                        <th class="w-40 text-center px-5">Product Name</th>
+                        <th class="w-40 text-center  px-5">Product Image</th>
+                        <th class="w-44 text-center  px-5">Product Quantity</th>
+                        <th class="w-40 text-center  px-5">Product Price</th>
+                        <th class="w-44 text-center  px-5">Product Subtotal</th>
+                    </tr>
+                <tbody>
+                    <tr class="border-b">
+                        <th class="py-4">1</th>
+                        <th>Samsung</th>
+                        <th></th>
+                        <th>5</th>
+                        <th>$100.00</th>
+                        <th>$500.00</th>
+                    </tr>
+                    <tr class="border-b">
+                        <th class="py-4">2</th>
+                        <th>Samsung</th>
+                        <th></th>
+                        <th>5</th>
+                        <th>$100.00</th>
+                        <th>$500.00</th>
+                    </tr>
+                    <tr class="border-b">
+                        <th class="py-4">3</th>
+                        <th>Samsung</th>
+                        <th></th>
+                        <th>5</th>
+                        <th>$100.00</th>
+                        <th>$500.00</th>
+                    </tr>
+                </tbody>
+                </thead>
+            </table>
+            <div class="flex justify-between">
+                <p class="m-5">Total Amount</p>
+                <p class="m-5 mr-14">$1500</p>
+            </div>
+            <div class="flex justify-between">
+                <p class="ml-5">Delivery Fee</p>
+                <p class="m-1 mr-14">$15</p>
+            </div>
+            <hr>
+            <div class="flex justify-between">
+                <p class="ml-5">Sub Total</p>
+                <p class="m-1 mr-14"> = $1515.00</p>
+            </div>
+        </div>
+        <button class="w-32 rounded-md bg-blue-700 p-2 m-5 float-right">Deliver</button>
+        <button class="w-32 rounded-md bg-slate-300 p-2 m-5 float-right">Cancel</button>
+
+
+
+    </main>
+
+
+
+</body>
+
+</html>
