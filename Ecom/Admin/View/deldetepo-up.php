7b6568a5767f06a3eb6e830ca6e03eb39799b271
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-07-28 02:21:15 +0630

    pop-up

---
+++ Ecom/Admin/View/deldetepo-up.php
@@ -0,0 +1,28 @@
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./resources/lib/tailwind/output.css" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="./resources/css/addCategory.css">
+</head>
+
+<body  class="flex justify-center h-screen  items-center ">
+    <div class="w-96 border rounded-xl shadow-xl">
+        <div class=" bg-blue-600 rounded-xl rounded-b-none">
+        <img class="py-9 px-16 " src="./resources/img/delete.png" alt="">
+        </div>
+        <div class="flex flex-col items-center space-y-5 mt-5 justify-center">
+            <p class="Font-bold text-3xl">Delete</p>
+            <p>Sure you want to delete</p>
+            <div class="space-x-5 pb-10">
+                <button class="bg-blue-600 rounded-lg px-7 py-2 shadow-2xl" >Delete</button>
+                <button class=" border rounded-lg bg-slate-400 px-7 py-2 shadow-xl">No</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
