a84da3422e3d8e7e4807d4f40697cfbb345936db
thetwaiyanhtet <soe.thi.ha2001@gmail.com>
2023-07-27 20:54:39 +0630

    js folder create

--- Ecom/Admin/View/adminsidebar.php
+++ Ecom/Admin/View/adminsidebar.php
@@ -13,7 +13,7 @@
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
-    <script src="./sidebar.js" defer></script>
+    <script src="./resources/js/sidebar.js" defer></script>
 </head>
 
 <body class=" font-poppins">
