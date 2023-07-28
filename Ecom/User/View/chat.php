e6930cb193cb1fafce73a5aa9df049e20f34c74d
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-07-28 20:09:35 +0630

    Update chat.php

--- Ecom/User/View/chat.php
+++ Ecom/User/View/chat.php
@@ -13,7 +13,6 @@
 </head>
 <body class="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
 
-	<!-- Component Start -->
 	<div class="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden">
 		<div class="flex flex-col flex-grow h-0 p-4 overflow-auto">
 			<div class="flex w-full mt-2 space-x-3 max-w-xs">
@@ -103,7 +102,7 @@
 			<input class="flex items-center h-10 w-full rounded px-3 text-sm" type="text" placeholder="Type your message…">
 		</div>
 	</div>
-	<!-- Component End  -->
+
 
 </body>
 
