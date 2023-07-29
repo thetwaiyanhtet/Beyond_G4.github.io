040d9a49032a27d9199ea7e9712f3ed25d6efb3a
thetwaiyanhtet <soe.thi.ha2001@gmail.com>
2023-07-29 22:57:08 +0630

    forget pw back arrow added

--- Ecom/Merchant/View/forgetPassword.php
+++ Ecom/Merchant/View/forgetPassword.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link href="./resources/lib/tailwind/output.css?id=<?=time() ?>" rel="stylesheet">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <link href="./resources/lib/tailwind/output.css?id=<?= time() ?>" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
@@ -14,23 +16,24 @@
 </head>
 
 <body>
-    <div class="flex justify-evenly items-center pt-36">
+    <a href=" ./signUp.php"><ion-icon name="arrow-back-outline" class="m-2 text-4xl p-3"></ion-icon></a>
+    <div class="flex justify-evenly items-center pt-28">
         <div>
             <img src="../View/resources/img/forget password img.png" alt="" class=" w-fit">
         </div>
         <div class="flex flex-col space-y-6">
             <div class=" text-center items-center flex flex-col justify-center space-y-5">
-            <div><img src="../View/resources/img/forget_password_people image.png" alt=""></div>
-            <div class=" font-poppins font-bold text-2xl w-[340px]">We will send you an email To reset your password.</div>
+                <div><img src="../View/resources/img/forget_password_people image.png" alt=""></div>
+                <div class=" font-poppins font-bold text-2xl w-[340px]">We will send you an email To reset your password.</div>
             </div>
-            <div class="text-[#667085] pt-5"> 
+            <div class="text-[#667085] pt-5">
                 <input type="text" class="w-[360px] h-[44px] border-[#d0d5dd67] border-2 rounded-md bg-white outline-none indent-2" placeholder="User@gmail.com">
-            </div>       
+            </div>
             <a href="./dashboard.php">
-            <button class="w-[360px] h-[44px] rounded-md bg-[#1366D9] flex items-center justify-center 
+                <button class="w-[360px] h-[44px] rounded-md bg-[#1366D9] flex items-center justify-center 
             font-PlayfairSC text-white">Get Started</button>
             </a>
-        
+
         </div>
     </div>
 </body>
