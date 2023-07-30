a89073a77b11831575cf077c9b6fb36ba7f9630e
thetwaiyanhtet <soe.thi.ha2001@gmail.com>
2023-07-30 18:09:43 +0630

    update

--- Ecom/User/View/userProfileEdit.php
+++ Ecom/User/View/userProfileEdit.php
@@ -67,7 +67,7 @@
                         </li>
                     </ul>
                 </div>
-               
+
     </nav>
     <main class="bg-gradient-to-tl from-[#d5a0e6] - to-[#9f80f4] dark:bg-linear_dark min-h-screen rounded-tl-[35px] rounded-tr-[35px]">
         <div class=" text-white my-5 ml-8">
@@ -78,19 +78,19 @@
             <div class=" col-start-1 col-span-3 space-y-5 lg:w-[700px] lg:mx-20 mx-10">
                 <div class=" grid grid-cols-2 justify-around font-poppins">
                     <p class=" text-white text-start lg:text-xl text-sm">Username</p>
-                    <input type="text" placeholder="Username" class="profileInputBox  col-start-2">
+                    <input type="text" placeholder="Username" class="profileInputBox rounded-md col-start-2">
                 </div>
                 <div class=" grid grid-cols-2">
                     <p class="font-poppins text-white text-start lg:text-xl text-sm">Name</p>
-                    <input type="text" placeholder="Name" class="profileInputBox col-start-2">
+                    <input type="text" placeholder="Name" class="profileInputBox rounded-md col-start-2">
                 </div>
                 <div class=" grid grid-cols-2">
                     <p class="font-poppins text-white text-start lg:text-xl text-sm">Email</p>
-                    <input type="text" placeholder="user****@mail.com" class="profileInputBox col-start-2">
+                    <input type="text" placeholder="user****@mail.com" class="profileInputBox rounded-md col-start-2">
                 </div>
                 <div class=" grid grid-cols-2">
                     <p class="font-poppins text-white text-start lg:text-xl text-sm">Phone Number</p>
-                    <input type="text" placeholder="09********" class="profileInputBox col-start-2">
+                    <input type="text" placeholder="09********" class="profileInputBox rounded-md col-start-2">
                 </div>
                 <div class=" grid grid-cols-2">
                     <p class="font-poppins lg:text-xl text-sm text-white">Gender</p>
@@ -105,7 +105,7 @@
                 </div>
                 <div class="grid grid-cols-2 items-center justify-center">
                     <p class=" text-white lg:text-xl text-sm font-poppins">Date of Birth</p>
-                    <input type="date" class=" lg:w-72 w-44 lg:p-3 p-2 rounded-lg lg:text-base text-sm">
+                    <input type="date" class=" lg:w-72 w-66 lg:p-3 p-2 rounded-lg lg:text-base text-sm">
                 </div>
             </div>
 
