156d7bc03676f3814311ac23debcb3147c3e863c
Zaw Phyo <snippyjustro@gmail.com>
2023-08-01 00:14:17 +0630

    Update model.php

--- Ecom/Merchant/Model/model.php
+++ Ecom/Merchant/Model/model.php
@@ -1,15 +1,17 @@
 <?php
 
-$hostname = "localhost";
-$port = "3306";
+$hostname = "mysql-db-do-user-14242552-0.b.db.ondigitalocean.com";
+$port = "25060";
 $dbname = "beyond_db";
-$uname = "root";
-$pwd = "";
+$uname = "doadmin";
+$pwd = "AVNS_8znaub1DRoUGMbF6KKE";
 
 //Connection
-$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$dbname",
-    $uname,$pwd);
+$pdo = new PDO(
+    "mysql:host=$hostname;port=$port;dbname=$dbname",
+    $uname,
+    $pwd
+);
 
 // set error exc
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
