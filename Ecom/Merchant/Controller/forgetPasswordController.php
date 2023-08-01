e8161890ec86631a603c0d82750ac2d2188c81b6
Hninaeinlatt <Hninaeinlatt438@gmail.com>
2023-08-02 00:43:26 +0630

    update

--- Ecom/Merchant/Controller/forgetPasswordController.php
+++ Ecom/Merchant/Controller/forgetPasswordController.php
@@ -1,48 +1,48 @@
 <?php
 session_start();
 // ini_set('display_errors', 1);
- include "./common/mailSender.php";
- include "./common/generatePwFunction.php";
-if(isset($_POST["resetPw"])){
-  
-   $email = $_POST["email"];
+include "./common/mailSender.php";
+include "./common/generatePwFunction.php";
+if (isset($_POST["resetPw"])) {
+
+  $email = $_POST["email"];
+  echo $email;
 
 
-   //DB connection
-   include "../Model/model.php";
+  //DB connection
+  include "../Model/model.php";
 
-   //check email is exit or not
+  //check email is exit or not
 
-   $sql = $pdo -> prepare(
+  $sql = $pdo->prepare(
     "SELECT * FROM m_admin WHERE email=:email"
-   );
-   $sql ->bindValue(":email", $email);
-   $sql -> execute();
+  );
+  $sql->bindValue(":email", $email);
+  $sql->execute();
 
-   $rowCount = $sql -> rowCount();
+  $rowCount = $sql->rowCount();
 
-   if($rowCount > 0){
+  if ($rowCount > 0) {
     $tokenPw = generateRandomPassword();
-     $sql = $pdo->prepare(
+    $sql = $pdo->prepare(
       "UPDATE m_admin SET password = :token WHERE email =:email"
-     );
-     $sql -> bindValue(":token", $tokenPw);
-     $sql ->bindValue(":email", $email);
-     $sql -> execute();
-     echo "Successfully update password";
+    );
+    $sql->bindValue(":token", password_hash($tokenPw, PASSWORD_DEFAULT));
+    $sql->bindValue(":email", $email);
+    $sql->execute();
+    echo "Successfully update password";
 
-     //send email 
-     $mail = new SendMail();
-     $mail -> sendMail(
-      $tokenPw,
+    //send email 
+    $mail = new SendMail();
+    $mail->sendMail(
+      $email,
       "reset password",
-      "<a href='https://www.apple.com'>Apple</a>"
-     );
-
-   }else{
+      $tokenPw
+    );
+  } else {
     echo "Email does not exit in database";
-   }
-
-}else{
-    // header("Location: ../View/404page.php");
+  }
+  header("Location: ../View/dashboard.php");
+} else {
+  header("Location: ../View/404page.php");
 }
