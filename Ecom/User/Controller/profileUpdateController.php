c872d0a232ae1948ac436d395cfb2ef008d54ae4
thetwaiyanhtet <soe.thi.ha2001@gmail.com>
2023-08-05 02:05:59 +0630

    userProfileEdit upload

---
+++ Ecom/User/Controller/profileUpdateController.php
@@ -0,0 +1,37 @@
+<?php 
+session_start();
+include "../Model/model.php";
+
+$userEmail = $_SESSION["user_ID"];
+$name = $_POST["name"];
+$phone_no = $_POST["phone_no"];
+$dob = $_POST["dob"];
+$gender = $_POST["gender"];
+$street = $_POST["street"];
+
+$sql = $pdo->prepare(
+    "UPDATE m_customer
+    SET 
+    name = :name,
+    phone = :phone_no,
+    birthday = :dob,
+    gender = :gender,
+    str = :street,
+    update_date = :updateDate WHERE email=:email;"
+);
+
+$sql->bindValue(":name",$name);
+$sql->bindValue(":phone_no", $phone_no);
+$sql->bindValue(":dob", $dob);
+$sql->bindValue(":gender", $gender);
+$sql->bindValue(":street", $street);
+$sql->bindValue(":updateDate", date("Y-m-d"));
+$sql->bindValue(":email", $userEmail);
+
+echo $userEmail;
+$sql->execute();
+
+$userData = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+header("Location: ../View/profileMenu.php");
+?>
--- Ecom/User/View/profileMenu.php
+++ Ecom/User/View/profileMenu.php
@@ -59,7 +59,7 @@
                 <button class=" text-black px-5 py-2 lg:text-lg text-base drop-shadow-xl rounded-md bg-white dark:bg-gray-500 font-PlayfairSC dark:text-white">Log out</button>
             </div>
         
-            <a href="../Controller/userListConroller.php">
+            <a href="../Controller/userProfileController.php">
                 <div class=" lg:w-[350px] lg:h-[400px] lg:flex-col justify-center items-center flex flex-wrap space-y-3 lg:text-base text-xs font-semibold">
                     <div class=" flex justify-around items-center lg:py-4 lg:px-2 py-2 border bg-white dark:bg-gray-500 dark:border-gray-500 rounded-xl shadow-xl mt-4 lg:w-[290px] w-[160px]">
                         <img src="./resources/img/manage-accounts.svg" alt="..." class=" lg:w-[30px] w-[20px]">
