b7b85606d89561978b34204ba508adc11fb58dc0
thetwaiyanhtet <soe.thi.ha2001@gmail.com>
2023-08-12 21:08:36 +0630

    profile pic

--- Ecom/User/View/user_history.php
+++ Ecom/User/View/user_history.php
@@ -1,5 +1,7 @@
 <?php
 include "../Controller/orderHistoryController.php";
+$userData = $_SESSION["user_data"];
+$verifyData = $_SESSION["verifyData"];
 
 ?>
 <!DOCTYPE html>
@@ -60,9 +62,14 @@ include "../Controller/orderHistoryController.php";
                         <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
                     </svg>
                 </button>
-                <button type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                <button type="button" class="flex mr-3 text-sm  rounded-full md:mr-0 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                     <span class="sr-only">Open user menu</span>
-                    <img class="w-8 h-8 rounded-full" src="./resources/img/bille.png" alt="user photo">
+                    <?php if ($verifyData["verify"] == 0) { ?>
+                        <img class="w-10 h-10 rounded-full" src="../../Storage/profile/profile.png" alt="...">
+                    <?php } else if ($verifyData["verify"] == 1) { ?>
+                        <img class="w-10 h-10 rounded-full" src="../../<?= $userData["p_picture"] ?>" alt="...">
+                    <?php } ?>
+                    <!-- <img class="w-8 h-8 rounded-full" src="./resources/img/bille.png" alt="..."> -->
                 </button>
                 <!-- Dropdown menu -->
                 <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
@@ -81,7 +88,7 @@ include "../Controller/orderHistoryController.php";
                             <a href="./orderNotification.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Notification</a>
                         </li>
                         <li>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log out</a>
+                            <a href="./login.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log out</a>
                         </li>
                     </ul>
                 </div>
