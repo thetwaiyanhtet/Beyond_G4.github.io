db3bf381a9a5e5a9a409efcbd1e46492575c2b15
Hninaeinlatt <Hninaeinlatt438@gmail.com>
2023-08-17 19:46:05 +0630

    update

---
+++ Ecom/User/View/numberVerify.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./resources/css/verify.css">
+    <script src="./resources/js/verify.js"></script>
+</head>
+<body>
+<h1>PIN code 1.0</h1>
+<div class="pin-code">
+	  <input type="number" maxlength="1" autofocus>
+    <input type="number" maxlength="1">
+	  <input type="number" maxlength="1">
+    <input type="number" maxlength="1">
+ </div>
+</body>
+</html>
