9250b68cb60aa9a9f57fa754143c315f1dfc9a7f
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-08-18 15:10:27 +0630

    Update confirmation.php

--- Ecom/User/View/confirmation.php
+++ Ecom/User/View/confirmation.php
@@ -164,50 +164,65 @@ $_SESSION["payment_method"] = $_POST['deli'];
     <div class="flex flex-col relative">
         <div class="hidden max-lg:flex w-4/5 max-lg:flex-col max-lg:mx-auto max-lg:items-center max-lg:justify-center  font-poppins">
             <div class=" border-b-4 border-button-color w-11/12 pb-5 text-3xl  max-lg:text-xl font-bold">Your order</div>
+            <?php foreach ($_SESSION["placeholdcart"] as $index => $chunk) { ?>
+                   
+                   <form action="../Controller/orderController.php" method="post">
             <div class="flex w-11/12 mt-10 border-b-4 border-button-color  justify-center items-center">
                 <div class="mb-10 ">
-                    <img class="w-32" src="./resources/img/cart shopping.png" alt="">
+                    <img class="w-32" src="../..<?= $chunk["p_one"] ?>" alt="">
                 </div>
                 <div class=" mx-5 flex flex-col ">
                     <div class="flex justify-between mb-5">
-                        <span>Dress</span>
-                        <span>$250</span>
+                        <span><?= $chunk["name"] ?></span>
+                        <!-- <span>$250</span> -->
                     </div>
                     <div>
-                        <span>M</span>
-                        <span>Natural</span>
-                        <span>1pc</span>
+                        <!-- <span>M</span> -->
+                        <span> $<?= $chunk["sellprice"] ?></span>
+                        <!-- <span>1pc</span> -->
                     </div>
                 </div>
             </div>
+            <?php } ?>
             <div class="flex mt-5 max-lg:text-sm  text-xl font-semibold">
                 <div class="flex justify-start mb-5">
                     <span class=" mr-28">Total Amount</span>
-                    <span>$260</span>
+                    <span><?=$_SESSION['delitotalamt']?></span>
                 </div>
             </div>
         </div>
+          
         <div class="w-11/12 h-80 max-lg:h-72 hidden max-lg:flex max-lg:flex-col max-lg:mx-auto max-lg:items-start max-lg:justify-center bg-gray-200 p-5 rounded-lg shadow-md">
             <p class=" font-bold ml-20 ">Checkout Details</p>
             <div class="mt-5 font-semibold max-lg:text-sm ">
-                <p>Hnin Eain Latt</p>
-                <p>324 Tang Noug, Kawthaung</p>
-                <span class="border-b-2 border-button-color pb-2">hninaeinlatt438@gmail.com</span>
+                <p><?= $addressandname[0]['name'] ?></p>
+                <p><?= $addressandname[0]['street'] ?></p>
+                <span class="border-b-2 border-button-color pb-2"><?= $addressandname[0]['email'] ?></span>
             </div>
             <div class="flex align-middle items-center mt-5">
-                <span class="border-b-2 border-button-color font-bold mb-5 pb-3">Royal Express</span>
-                <p class=" text-xs">(Expected delivery on 30/07/23)</p>
+                <span class="border-b-2 border-button-color font-bold pb-3">Royal Express</span>
+                <p class=" text-xs">(Expected delivery on <?php
+                                                                $currentDate = new DateTime();
+                                                                $currentDate->add(new DateInterval('P3D'));
+                                                                $newDate = $currentDate->format('d/m/Y');
+                                                                echo $newDate;
+                                                                ?>)</p>
             </div>
-            <button class="px-4 py-3 mt-2 bg-button-color rounded-md flex justify-center items-center">
+            <button class="px-4 py-3 mt-2 bg-[#314755]  rounded-md flex justify-center items-center">
                 <label class="flex text-white space-x-2">
-                    <input type="radio" name="deli" class="form-checkbox w-6 h-6  ">
-                    <span>Credit Card</span>
+                <?php if ($_POST['deli'] == 0) { ?>
+                            <span>Visa</span>
+                      <?php  } ?>
+                      <?php if ($_POST['deli'] == 1) { ?>
+                            <span>KBZ Pay</span>
+                      <?php  } ?>
                 </label>
             </button>
         </div>
         <div class=" absolute hidden max-lg:flex -bottom-20 right-0 max-lg:-bottom-10 max-lg:right-5">
-            <button class=" text-white py-1 px-5 rounded-md bg-button-color">Next Step ></button>
+            <input type="submit" value="Check Out" class=" text-white py-1 px-5 rounded-md bg-[#314755] "></input>
         </div>
+        </form>
     </div>
 </body>
 
