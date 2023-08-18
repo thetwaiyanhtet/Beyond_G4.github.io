37d6ace5cfe202321e62f80891f042ba8ffc6a29
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-08-18 20:47:12 +0630

    update

--- Ecom/User/Controller/newcartController.php
+++ Ecom/User/Controller/newcartController.php
@@ -2,50 +2,53 @@
 session_start();
 include "../Model/model.php";
 $carted = [];
-if (isset($_POST['product_id']) ) {
-    print_r($_POST['product_id']) ;
-    if (isset($_SESSION['usercart'])) {
-        // unset($_SESSION['usercart']);
-        $item_array_id = array_column($_SESSION['usercart'], "product_id");
+ 
+    # code...
 
-        if (false) {
-            // echo "<script>alert('Product is already added in the cart')</script>";
-            // echo "<script> window.location = '../View/mainPage.php'  </script>";
+    if (isset($_POST['product_id'])) {
+        print_r($_POST['product_id']);
+        if (isset($_SESSION['usercart'])) {
+            // unset($_SESSION['usercart']);
+            $item_array_id = array_column($_SESSION['usercart'], "product_id");
 
+            if (false) {
+                // echo "<script>alert('Product is already added in the cart')</script>";
+                // echo "<script> window.location = '../View/mainPage.php'  </script>";
+
+            } else {
+                $count =  count($_SESSION['usercart']);
+                $item_array = array(
+                    'product_id' => $_POST['product_id']
+
+                );
+
+                $_SESSION['usercart'][$count] = $item_array;
+            }
         } else {
-            $count =  count($_SESSION['usercart']);
             $item_array = array(
                 'product_id' => $_POST['product_id']
-
             );
-
-            $_SESSION['usercart'][$count] = $item_array;
+            $_SESSION['usercart'][0] = $item_array;
         }
-    } else {
-        $item_array = array(
-            'product_id' => $_POST['product_id']
-        );
-        $_SESSION['usercart'][0] = $item_array;
-    }
 
 
-function uniqueAssoc($array)
-{
-    $result = array();
-    foreach ($array as $key => $value) {
-        if (!in_array($value, $result)) {
-            $result[$key] = $value;
+        function uniqueAssoc($array)
+        {
+            $result = array();
+            foreach ($array as $key => $value) {
+                if (!in_array($value, $result)) {
+                    $result[$key] = $value;
+                }
+            }
+            return $result;
         }
+        $cartass = $_SESSION['usercart'];
+
+        $uniqueArray = uniqueAssoc($cartass);
+
+        $cart =  $uniqueArray;
+
+        $_SESSION['carttle'] = $cart;
     }
-    return $result;
-}
-$cartass = $_SESSION['usercart'];
-
-$uniqueArray = uniqueAssoc($cartass);
-
-$cart =  $uniqueArray;
-
-$_SESSION['carttle'] = $cart;
-}
 
 // header("Location: ../View/mainPage.php");
