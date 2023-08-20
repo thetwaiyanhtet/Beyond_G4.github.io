8826c79ed6627abada29a91d361e41cb212a2e4c
nyanwinmyo <nyanwinmyo@gmail.com>
2023-08-20 23:30:07 +0630

    update

--- Ecom/Admin/Controller/categoryListController.php
+++ Ecom/Admin/Controller/categoryListController.php
@@ -12,6 +12,19 @@ $data=$sql->fetchAll(PDO::FETCH_ASSOC);
 
 $_SESSION["m_category"]=$data;
 
+$subSql = $pdo->prepare(
+    "SELECT t_sub_category.category_id,t_sub_category.s_c_name 
+    FROM t_sub_category
+     JOIN m_admin_category
+      ON t_sub_category.category_id=m_admin_category.id
+      ;"
+);
+$subSql->execute(); //run real sql
+
+$subData=$subSql->fetchAll(PDO::FETCH_ASSOC);
+
+$_SESSION["sub_category"]=$subData;
+
 //DB Connection
 
 
