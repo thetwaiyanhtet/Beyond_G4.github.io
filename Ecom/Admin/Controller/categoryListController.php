f47c9289da7f692f4a5444c0fefa94d032e4d8c9
nyanwinmyo <nyanwinmyo@gmail.com>
2023-08-21 12:13:44 +0630

    update

--- Ecom/Admin/Controller/categoryListController.php
+++ Ecom/Admin/Controller/categoryListController.php
@@ -4,26 +4,37 @@
 include "../Model/model.php";
 
 $sql = $pdo->prepare(
-    "SELECT * FROM m_admin_category;"
+    "SELECT m_admin_category.c_name, m_admin_category.id, t_sub_category.category_id,
+    GROUP_CONCAT(COALESCE(t_sub_category.s_c_name, '')) AS sub_category_names, m_admin_category.create_date
+    FROM m_admin_category
+    LEFT JOIN t_sub_category ON t_sub_category.category_id = m_admin_category.id
+    GROUP BY m_admin_category.id;"
 );
-$sql->execute(); //run real sql
+$sql->execute();
+$data = $sql->fetchAll(PDO::FETCH_ASSOC);
+foreach ($data as &$row) {
+    if (empty($row['sub_category_names'])) {
+        $row['sub_category_names'] = "-";
+    }
+}
 
-$data=$sql->fetchAll(PDO::FETCH_ASSOC);
+$_SESSION["m_category"] = $data;
 
-$_SESSION["m_category"]=$data;
 
-$subSql = $pdo->prepare(
-    "SELECT t_sub_category.category_id,t_sub_category.s_c_name 
-    FROM t_sub_category
-     JOIN m_admin_category
-      ON t_sub_category.category_id=m_admin_category.id
-      ;"
-);
-$subSql->execute(); //run real sql
+// $subSql = $pdo->prepare(
+//     "SELECT t_sub_category.category_id, GROUP_CONCAT(t_sub_category.s_c_name) AS sub_category_names
+//     FROM t_sub_category
+//     JOIN m_admin_category
+//     ON t_sub_category.category_id = m_admin_category.id
+//     GROUP BY t_sub_category.category_id;"
+// );
 
-$subData=$subSql->fetchAll(PDO::FETCH_ASSOC);
+// $subSql->execute(); // Run the SQL query
 
-$_SESSION["sub_category"]=$subData;
+// $subData = $subSql->fetchAll(PDO::FETCH_ASSOC);
+
+// $_SESSION["sub_category"] = $subData;
+
 
 //DB Connection
 
