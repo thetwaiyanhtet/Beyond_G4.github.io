b4a77805935c05628cbcdc4272378d8b9a45053c
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-08-21 20:37:24 +0630

    update logo and name

--- Ecom/Admin/View/adminsidebar.php
+++ Ecom/Admin/View/adminsidebar.php
@@ -92,27 +92,7 @@ include "../Controller/logoandbusinessController.php";
                             <a class="py-2 px-16 act block text-sm hover:bg-table hover:text-white categoryl " href="./categorylist.php">Category List</a>
                             <a class="py-2 px-16 block text-sm  hover:bg-table hover:text-white categorya " href="./addcategory.php">Add Category</a>
                         </div>
-                    <!-- </div>
-                    <div x-data="{ open: false }">
-                        <button @click="open = !open" class="w-full  flex my-2 justify-between items-center py-3 px-6 text-black cursor-pointer rounded-r-3xl hover:bg-table hover:text-white focus:outline-none merchant">
-                            <span class="flex items-center ">
-                                <ion-icon class="h-5 w-5" name="person-outline"></ion-icon>
-                                <span class="mx-4 font-medium">Merchant</span>
-                            </span>
 
-                            <span>
-                                <svg class="h-4 w-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path x-show="! open" d="M9 5L16 12L9 19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="display: none;"></path>
-                                    <path x-show="open" d="M19 9L12 16L5 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                </svg>
-                            </span>
-                        </button>
-
-                        <div x-show="open" class="bg-gray-100">
-                            <a class="py-2 px-16 block text-sm text-gray-600 hover:bg-table hover:text-white merchantList" href="./merchant_list.php">Merchant List and Profile</a>
-                            <!-- <a class="py-2 px-16 block text-sm text-gray-600 hover:bg-table hover:text-white merchantApprove" href="./merchant_approve.php">Merchant Approve</a> -->
-                        <!-- </div>
-                    </div> -->
                     <div x-data="{ open: false }">
                         <a href="./merchant_list.php">
                             <button @click="open = !open" class="w-full flex my-2 justify-between items-center py-3 px-6  cursor-pointer rounded-r-3xl hover:bg-table hover:text-white focus:outline-none merchant">
