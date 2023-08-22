7dfcbc7a721a43efc5df94206073e92b0c186180
Yunn Shwe Yi Win <yunnshweyiwin37@gmail.com>
2023-08-23 01:42:28 +0630

    ..

--- Ecom/User/View/navigation.php
+++ Ecom/User/View/navigation.php
@@ -1,22 +1,73 @@
+<?php
+
+include "../Controller/categoryListController.php";
+include "../Controller/newcartController.php";
+include "../Controller/wishlistcart.php";
+include "../Controller/trendingProductController.php";
+include  "../../User/Controller/bannerController.php";
+include  "../../Admin/Controller/readfaqController.php";
+include "../Controller/userProfileController.php";
+include "../Controller/testinguserProfileController.php";
+$faq = $_SESSION["m_faq"];
+include "../Controller/bannerController.php";
+$banner = $_SESSION["banner"];
+$userData = $_SESSION["user_data"];
+$verifyData = $_SESSION['verifyData'];
+$customer = $_SESSION['checkEmail']
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="icon" href="./resources/img/logo_upt.png" type="image/png" sizes="16x16">
+    <title>Beyond</title>
+    <link href="./resources/lib/tailwind/output.css?id=<?= time() ?>" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Philosopher:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=BioRhyme&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,500&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./resources/css/scroll.css">
+    <link rel="stylesheet" href="./resources/css/bannerSlideShow.css">
+    <link rel="stylesheet" href="./resources/css/chat.css">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script src="../View/resources/lib/jquery3.6.0.js"></script>
+    <script src="./resources/js/bannerSlideShow.js" defer></script>
+    <script src="./resources/js/searchProduct.js" defer></script>
+    <script src="./resources/js/userMainPage.js" defer></script>
+    <script src="./resources/js/chat.js" defer></script>
+    <script>
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    </script>
+
 </head>
 
 <body>
-    <nav class=" bg-violet-300 dark:bg-color-primary-dark fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <nav class=" bg-violet-300 dark:bg-color-primary-dark fixed w-full z-40 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
         <div class=" flex flex-wrap items-center justify-between mx-auto p-4 ">
             <a href="./mainPage.php" class="flex items-center">
                 <img src="./resources/img/logo_slowdown.gif" class="mr-3 h-12" alt="beyond Logo" />
                 <span class="text-2xl font-semibold whitespace-nowrap text-black dark:text-white font-philosopher">Beyond</span>
             </a>
 
-            <div class="flex items-center md:order-2">
 
+            <div class="flex items-center md:order-2 justify-between">
+                <div class="relative w-32 -translate-y-4">
+                    <div class=" overflow-hidden absolute mx-2 rounded-xl">
+                        <div id="ln_space" class="w-28 h-20"></div>
+                    </div>
+                </div>
                 <input type="checkbox" name="cart" id="cart" class="hidden peer " />
                 <label for="cart" class="ml-3">
                     <div class="relative  ">
@@ -39,6 +90,7 @@
                         </div>
                     </div>
                 </label>
+
                 <!-- <label for="cart" class="ml-3"><ion-icon name="cart-outline" class="text-2xl mt-2 dark:text-white text-black"></ion-icon></label> -->
                 <button id="theme-toggle" type="button" class="text-gray-900 dark:text-white focus:outline-none  rounded-lg text-sm p-2.5">
                     <svg id="theme-toggle-dark-icon" class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
@@ -126,7 +178,7 @@
             <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 bg-transparent" id="navbar-user">
                 <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:border-gray-700">
                     <li>
-                        <a href="./mainPage.php" class="block py-2 pl-3 pr-4 text-purple-800 bg-purple-400 rounded md:bg-transparent md:text-white md:p-0 md:dark:text-purple-500 font-bold" aria-current="page">Home</a>
+                        <a href="./mainPage.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-600 md:p-0 dark:text-white md:dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-purple-500 md:dark:hover:bg-transparent dark:border-gray-700 font-bold" aria-current="page">Home</a>
                     </li>
                     <li>
                         <a href="./aboutUs.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-600 md:p-0 dark:text-white md:dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-purple-500 md:dark:hover:bg-transparent dark:border-gray-700 font-bold">About Us</a>
@@ -164,11 +216,43 @@
             </div>
         </div>
     </div>
+
+
     <button class="chat-btn z-50 scale-110" id="chatBoxButton">
         <img src="./resources/img/chat.png" alt="" class="w-20 h-auto animate-[wiggle_700ms_ease-in-out_infinite] ">
     </button>
     <div class="chat-popup z-50 bg-white">
     </div>
+
+
+    <button id="to-top-button" onclick="goToTop()" title="Go To Top" class="hidden fixed z-50 bottom-28 right-10 border-0 w-14 h-14 rounded-full drop-shadow-md shadow-inner bg-purple-500 text-white text-3xl font-bold "><ion-icon name="arrow-up-outline" class="scale-110"></ion-icon></button>
 </body>
+<script src="//cdn.conveythis.com/javascript/conveythis-initializer.js"></script>
+<script>
+    document.addEventListener("DOMContentLoaded", function(e) {
+        ConveyThis_Initializer.init({
+            api_key: "pub_f0dadebebe17f7b23e125a0a04edb015"
+        });
+    });
+</script>
+<script>
+    var toTopButton = document.getElementById("to-top-button");
+    window.onscroll = function() {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            toTopButton.classList.remove("hidden");
+        } else {
+            toTopButton.classList.add("hidden");
+        }
+    }
+
+    function goToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+</script>
+<script src="./resources/js/toggle.js" defer></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
 
 </html>
