f6ea9bd0e12a9e125e1d738183cf7162025c703b
nyanwinmyo <nyanwinmyo@gmail.com>
2023-08-24 20:25:01 +0630

    update

--- Ecom/Merchant/View/header.php
+++ Ecom/Merchant/View/header.php
@@ -16,7 +16,11 @@ $data = $_SESSION["verifyData"];
     <div class=" ml-56 ">
         <header class=" border-gray-300 border-b-2 h-[82px] flex justify-between items-center z-30">
             <div class=" pl-5">
-           
+            <?php if(isset($merchant_data["store_name"])){ ?> 
+                <p class=" font-semibold"><?= $merchant_data["store_name"] ?></p>
+                <?php } else{ ?> 
+                    <p class=" font-semibold"></p>
+                    <?php } ?>
                 <p class=" text-sm" id="date_time"></p>
             </div>
             <div>
