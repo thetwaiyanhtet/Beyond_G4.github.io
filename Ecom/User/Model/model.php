5ece3103e767a7f78c7a27133469adc3087779cf
nyanwinmyo <nyanwinmyo@gmail.com>
2023-08-27 23:54:47 +0630

    ...

--- Ecom/User/Model/model.php
+++ Ecom/User/Model/model.php
@@ -1,5 +1,4 @@
 <?php
-
 $hostname = "hosting-beyond-do-user-14484910-0.b.db.ondigitalocean.com";
 $port = 25060;
 $dbname = "beyond_db";
@@ -7,17 +6,9 @@ $uname = "doadmin";
 $pwd = "AVNS_odSp7G5IWykGCI5O0CB";
 
 //Connection
-$pdo = new PDO(
-    "mysql:host=$hostname;
-    port=$port;
-    dbname=$dbname",
-    $uname,
-    $pwd
-);
+$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$dbname",
+    $uname,$pwd);
 
 // set error exc
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-
-?>
+ ?>
